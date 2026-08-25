b192165ee3658336fe81876b71ff207eca24a559
Aaryan Shrivastava <136842195+iamaaryan10@users.noreply.github.com>
2026-08-26 00:25:37 +0530

    Learining Field Validator

--- pydantic_learning/02_filed_validator.py
+++ pydantic_learning/02_filed_validator.py
@@ -1,6 +1,8 @@
 from pydantic import BaseModel, EmailStr, AnyUrl, Field, field_validator
 from typing import List, Dict, Optional, Annotated
 
+# Field validator can be used on a single field and if more than one field is to be validated at once or dependent on each other we use model validator
+
 class Patient(BaseModel):
 
     name : str
@@ -21,6 +23,19 @@ class Patient(BaseModel):
             raise ValueError("Not a valid domain")
         return value
 
+    @field_validator('name') # This validator will convert the name to uppercase
+    @classmethod
+    def name_transform(cls, name):
+        return name.upper()
+
+    @field_validator('age', mode='before') # before mode will get the value before type coersion 
+    @classmethod
+    def age_limit(cls, val):
+        if 0 < val <= 100:
+            return val
+        else:
+            raise ValueError("Age not in limit")
+
 def update_patient(patient: Patient):
 
     print(patient.name)
@@ -30,7 +45,7 @@ def update_patient(patient: Patient):
 
 patient_info = {'name':'Aaryan', 
                 'email' : "abc@hdfc.com",
-                'age' : '22', 
+                'age' : 22, 
                 'weight' : 60.2, 
                 'married' : True ,
                 'allergies' : ['cough', 'fever'], 
