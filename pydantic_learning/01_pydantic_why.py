b9a7e222ed55aa65dcdd931abe9bac23d0038c17
Aaryan Shrivastava <136842195+iamaaryan10@users.noreply.github.com>
2026-08-25 19:04:04 +0530

    Learning pydantic

--- pydantic_learning/01_pydantic_why.py
+++ pydantic_learning/01_pydantic_why.py
@@ -1,20 +1,36 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
+from typing import List, Dict, Optional
 
 class Patient(BaseModel):
+
     name : str
+    email : EmailStr
     age : int
+    weight : float
+    married : bool = False # Default is false
+    allergies : Optional[List[str]] = None # Optional and Default is none
+    contact_details : Dict[str, str]
 
 def insert_patient_data(patient: Patient):
+
     print(patient.name)
     print(patient.age)
     print("Inserted")
 
 def update_patient_data(patient: Patient):
+
     print(patient.name)
     print(patient.age)
     print("updated")
 
-patient_info = {'name':'Aaryan', 'age' : '22'}
+patient_info = {'name':'Aaryan', 
+                'email' : "abc@gmail.com",
+                'age' : '22', 
+                'weight' : 60.2, 
+                'married' : True ,
+                'allergies' : ['cough', 'fever'], 
+                'contact_details' : {'Phone' : '95959595', 'email' : 'aaryan@123'}
+                }
 
 patient1 = Patient(**patient_info)
 
