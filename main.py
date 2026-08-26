19c103e58e7fc20d06f138c1559440628a295b74
Aaryan Shrivastava <136842195+iamaaryan10@users.noreply.github.com>
2026-08-26 23:38:47 +0530

    Learning PUT

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, Path, HTTPException, Query
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel, Field, computed_field
-from typing import Annotated, Literal
+from typing import Annotated, Literal, Optional
 import json
 
 app = FastAPI()
@@ -33,6 +33,15 @@ class Patient(BaseModel):
             return 'Normal'
         else:
             return 'Obese'
+
+class PatientUpdate(BaseModel):
+
+    name : Annotated[Optional[str], Field(default=None)]
+    city: Annotated[Optional[str], Field(default=None)]
+    age : Annotated[Optional[int], Field(default=None, gt=0, lt=121,)]
+    gender : Annotated[Optional[Literal['male', 'female', 'other']], Field(default=None)]
+    height : Annotated[Optional[float], Field(default=None, gt = 0)]
+    weight : Annotated[Optional[float], Field(default=None, gt = 0)]
 
 def load_data():
     with open("patients.json", "r") as f:
@@ -101,3 +110,30 @@ def create_patient(patient: Patient):
     save_data(data)
 
     return JSONResponse(status_code=201, content={'message' : 'Patient created successfully'})
+
+
+@app.put('/edit/{patient_id}')
+def update_patient(patient_id : str, patient_update: PatientUpdate):
+
+    data = load_data()
+
+    if patient_id not in data:
+        raise HTTPException(status_code=404, detail='Patient ID not found')
+
+    exisiting_patient_info = data[patient_id]
+
+    update_patient_info = patient_update.model_dump(exclude_unset=True)
+
+    for key, value in update_patient_info.items():
+        exisiting_patient_info[key] = value
+
+    exisiting_patient_info['id'] = patient_id
+    patient_pydantic_object = Patient(**exisiting_patient_info)
+    exisiting_patient_info = patient_pydantic_object.model_dump(exclude='id')
+
+    data[patient_id] = exisiting_patient_info
+
+    #save data
+    save_data(data)
+
+    return JSONResponse(status_code=200, content={"message": "patient updated"})
